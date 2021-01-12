a2418704312bd7541d362f8e0fa490fa677393a7
Gael Porteu de La Morandière <gael.porteu@gmail.com>
2021-01-12 17:16:31 -0500

    Linked js to html file and created factory object

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
+// Politician factory function object
+let newPolitian = function (name) {
+// Politician object
 let politician = {
-    nameFull: "",
+    nameFull: name,
     electionResults: null,
     totalVotes: 0
   };
+  // The return of the object
+  return politician;
+}
+// First new politician object
+let hab = newPolitian("Carey Price");
+// Second new politician object
+let leaf = newPolitian("Frederik Andersen")
